bec0a535c08430fb35a820f504afdfd2faeadbbf
PainappuruNyan <painappuru369@gmail.com>
2022-03-11 02:18:13 +1000

    api weather, news tg bot

--- apiTest.py
+++ apiTest.py
@@ -5,23 +5,25 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 news_api = config.news_api
-api_key = config.api_key
+weather_api = config.weather_api
 bot = Bot(token=config.bot_token)
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(content_types=["text"])
-async def daily(message: types.Message):
-    geo = requests.get("http://ipwhois.app/json/").json()
+async def forecast_news_notification(message: types.Message) -> None:
+    """Сбор данных о погоде и новостях на основе местоположения с последующей отправкой в тг бота"""
+
+    geo = requests.get("http://ipwhois.app/json/").json()  # Определение местоположения пользователя
     country = geo["country"]
     news = requests.get(f"https://newsapi.org/v2/everything?q={country}&language=ru&apiKey={news_api}").json()[
         "articles"
-    ]
+    ]  # Поиск последних новостей по стране пользователя
     lat = geo["latitude"]
     lon = geo["longitude"]
     city_weather = requests.get(
-        f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang=ru&appid={api_key}"
-    ).json()
+        f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang=ru&appid={weather_api}"
+    ).json()  # Сбор информации о погоде в городе пользователя
     await bot.send_message(
         message.from_user.id,
         f"{city_weather['weather'][0]['description']}\nтемпература воздуха: {city_weather['main']['temp'] - 273.15}\n"
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import keyring
 
 bot_token = keyring.get_password("api", "telegram")
-api_key = "92b662a37f5ca90b4d5980e86c60969e"
-news_api = "9ff9076dfde54defa425c50390bb6511"
+weather_api = keyring.get_password("api", "weather")
+news_api = keyring.get_password("api", "news")
