0fc1521b6c728571586ae7a00f15ea949d12ede4
PainappuruNyan <painappuru369@gmail.com>
2022-03-10 13:26:09 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-bot_token = '5116812416:AAFBserS0QBcObbnVEdk9SZIsu8t0_Ujwvc'
-api_key = '92b662a37f5ca90b4d5980e86c60969e'
-news_api = '9ff9076dfde54defa425c50390bb6511'
+import keyring
+
+bot_token = keyring.get_password("api", "telegram")
+api_key = "92b662a37f5ca90b4d5980e86c60969e"
+news_api = "9ff9076dfde54defa425c50390bb6511"
